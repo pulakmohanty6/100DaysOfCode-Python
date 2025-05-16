3ce155fd4d54df48ed0af20a9ea7494e2eaa121a
Pulak Mohanty <166727790+pulakmohanty6@users.noreply.github.com>
2025-05-16 19:33:58 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+place = input("What's the name of the city you grew up in?\n")
+name = input("What's your pet name?\n")
+print("Your band name could be " + place + " " + name)
+
+
